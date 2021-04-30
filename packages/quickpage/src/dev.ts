4dea5fdf9e743176f49cde69740c8b5c4fd130ff
junwen <junwen.su@shopee.com>
2021-04-30 22:59:55 +0800

    fix: 🐛 run dev can not find dir error

--- packages/quickpage/src/dev.ts
+++ packages/quickpage/src/dev.ts
@@ -1,26 +1,29 @@
 import { readdirSync } from 'fs-extra';
 import { createServer } from 'vite';
-import { prompt } from './utils';
+import { prompt, info } from './utils';
 import { join } from 'path';
 
 export const dev = async () => {
-  // TODO: 判断是否是文件夹
-  const projectNames = readdirSync('pages');
+  try {
+    const projectNames = readdirSync('pages');
 
-  const projectName = await prompt({
-    title: 'Select Project',
-    choices: projectNames.map(name => ({ name })),
-  });
+    const projectName = await prompt({
+      title: 'Select Project',
+      choices: projectNames.map((name: string) => ({ name })),
+    });
 
-  const cwd = process.cwd();
+    const cwd = process.cwd();
 
-  const server = await createServer({
-    // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
-    configFile: false,
-    root: join(cwd, 'pages', projectName.value),
-    server: {
-      port: 3333,
-    },
-  });
-  await server.listen();
+    const server = await createServer({
+      // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
+      configFile: false,
+      root: join(cwd, 'pages', projectName.value),
+      server: {
+        port: 3333,
+      },
+    });
+    await server.listen();
+  } catch (err) {
+    info(String(err), 'ERROR');
+  }
 };
